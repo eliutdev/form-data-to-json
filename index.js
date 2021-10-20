fd38a8b24b575933579657c4b1a9de3bda125394
Eliut González <63687573+eliutgon@users.noreply.github.com>
2021-10-20 18:54:03 +0200

    chore: initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const form = document.forms['users']
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    console.log(formDataToJSON())
+}
+
+function formDataToJSON() {
+    const user = {}
+    Array.from(form.elements).forEach(element => {
+        if (element.name) user[element.name] = element.value
+    })
+    return user
+}
